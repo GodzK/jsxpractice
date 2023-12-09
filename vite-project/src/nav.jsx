2159e19ea65b5c8cc56f34490167a69da29c68fd
GodzK <64467457+GodzK@users.noreply.github.com>
2023-12-09 12:46:28 +0700

    first website
    first website that make with react that can do with myself

--- vite-project/src/nav.jsx
+++ vite-project/src/nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import reactLogo from './assets/react.svg'
 import './App.css'
 function nav() {
@@ -6,13 +6,12 @@ function nav() {
     <div className='nav'>
       <div className='logo'>
         <img src={reactLogo}></img>
-        <h1>Phakaphol the king</h1>
+        <h3>ReactFacts</h3>
         </div>
       <ul className='nav-item'>
     <li><a href="#home">Home</a></li>
     <li><a href="#news">News</a></li>
     <li><a href="#contact">Contact</a></li>
-    <li><a href="#about">About</a></li>
   </ul>
   <footer></footer>
   </div>
